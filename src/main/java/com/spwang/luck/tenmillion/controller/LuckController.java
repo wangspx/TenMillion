33875cc5a6462b312289c62dc8d4073bb15ea4bd
spwang <spwang@live.cn>
2019-09-09 21:44:21 +0800

    test

---
+++ src/main/java/com/spwang/luck/tenmillion/controller/LuckController.java
@@ -0,0 +1,15 @@
+package com.spwang.luck.tenmillion.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author spwang on 2019-09-09 at 21:42
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+@RestController
+@RequestMapping("/luck")
+public class LuckController implements VersionController {
+
+}
